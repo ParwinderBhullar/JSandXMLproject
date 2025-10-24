bce8b22dba95bd5f62f58dd03f31c159ad18326a
ParwinderBhullar <108417998+ParwinderBhullar@users.noreply.github.com>
2025-10-24 12:07:08 -0400

    Add Google API configuration to config.js

---
+++ js/config.js
@@ -0,0 +1,7 @@
+
+const GOOGLE_CONFIG = {
+  CLIENT_ID: "Your id here",
+  API_KEY: "Your key",
+  DISCOVERY_DOC: "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
+  SCOPES: "https://www.googleapis.com/auth/calendar.readonly"
+};
